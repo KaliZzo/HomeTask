0088e1b9dcd3f8d1d226bd28dc5f26329ebcdde8
Nisim Ohana <nisim@jeen.ai>
2025-01-24 16:50:26 +0200

    backend mission 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const path =require('path')
 const port = 3001
 
 
@@ -7,7 +8,7 @@ const API_Key =process.env.PixaBay_API
 
 
 app.get('/',(req,res)=>{
-    res.send('server is running (mission 1)')
+    res.sendFile(path.join(__dirname, 'main.html'))
 })
 
 app.listen(port, ()=>{
